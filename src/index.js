5366fc1adcc4305cbc67661e341b7dc3564a768e
Hossam Elbahrawy <hossam@hayasolutions.com>
2019-11-15 03:19:15 +0200

    connected to mongodb

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+const mongoUri = 'mongodb://127.0.0.1:27017';
 
 app.get('/', (req, res) => {
   res.send('Hi there!');
+});
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to MongoDb');
+});
+mongoose.connection.on('Error', err => {
+  console.log('Error connectiong to MongoDb', err);
 });
 
 app.listen(3000, () => {
